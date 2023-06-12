e8e45cf24937de75b9988aa33b981e0cb6394fb1
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-06-12 17:08:50 +0500

    created controllers

---
+++ src/main/java/com/asldev/uz/roombookingapi/controller/BookingController.java
@@ -0,0 +1,55 @@
+package com.asldev.uz.roombookingapi.controller;
+
+import com.asldev.uz.roombookingapi.service.BookingService;
+import com.asldev.uz.roombookingapi.service.dto.BookingDto;
+import com.asldev.uz.roombookingapi.service.exception.SuccessMessage;
+import com.asldev.uz.roombookingapi.service.utils.Availability;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/rooms")
+public class BookingController {
+    private final BookingService bookingService;
+
+    @Autowired
+    public BookingController(BookingService bookingService){
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping("/{roomId}/availability")
+    private ResponseEntity<List<Availability>> getAvailableBookingTimes(@PathVariable Long roomId,
+                                                                       @RequestParam(name = "date", required = false)
+                                                            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDate date)
+    {
+        List<Availability> availableBookingTimes = bookingService.getAvailableBookingTimes(roomId, date);
+        return new ResponseEntity<>(availableBookingTimes, HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}/book")
+    private ResponseEntity<SuccessMessage> bookRoom(@PathVariable Long id,
+                                                    @RequestBody BookingDto roomDto)
+    {
+        SuccessMessage successMessage = bookingService.bookRoom(id, roomDto);
+        return new ResponseEntity<>(successMessage, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}/bookings")
+    private ResponseEntity<List<BookingDto>> findAll(@PathVariable Long id)
+    {
+        List<BookingDto> all = bookingService.findAll(id);
+        return new ResponseEntity<>(all, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/bookings/{id}")
+    private void delete(@PathVariable Long id){
+        bookingService.delete(id);
+    }
+}
---
+++ src/main/java/com/asldev/uz/roombookingapi/controller/RoomController.java
@@ -0,0 +1,58 @@
+package com.asldev.uz.roombookingapi.controller;
+
+import com.asldev.uz.roombookingapi.enums.RoomType;
+import com.asldev.uz.roombookingapi.service.RoomService;
+import com.asldev.uz.roombookingapi.service.dto.PageDto;
+import com.asldev.uz.roombookingapi.service.dto.RoomDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/rooms")
+public class RoomController {
+    private final RoomService roomService;
+
+    @Autowired
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    private ResponseEntity<PageDto> findAll(@RequestParam(required = false) String search,
+                                            @RequestParam(required = false) RoomType type,
+                                            @RequestParam(defaultValue = "0") int pageNumber,
+                                            @RequestParam(defaultValue = "10") int pageSize){
+        PageDto all = roomService.findAll(search, type, pageNumber, pageSize);
+        return new ResponseEntity<>(all, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    private ResponseEntity<Object> findById(@PathVariable Long id){
+        RoomDto roomDto = roomService.findById(id);
+        return new ResponseEntity<>(roomDto, HttpStatus.OK);
+
+    }
+    @PostMapping("/create")
+    private RoomDto create(@RequestBody RoomDto roomDto)
+    {
+        return roomService.create(roomDto);
+    }
+
+
+    @PutMapping("update/{id}")
+    private RoomDto update(@PathVariable Long id,
+                           @RequestBody RoomDto roomDto)
+    {
+        return roomService.update(id, roomDto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    private void delete(@PathVariable Long id){
+        roomService.delete(id);
+    }
+
+}
+
