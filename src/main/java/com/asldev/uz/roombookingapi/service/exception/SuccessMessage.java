4af983a24ccf2f01fd9799e57c4049cad2a8260f
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-06-12 09:22:30 +0500

    created SuccessMessage

---
+++ src/main/java/com/asldev/uz/roombookingapi/service/exception/SuccessMessage.java
@@ -0,0 +1,14 @@
+package com.asldev.uz.roombookingapi.service.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SuccessMessage {
+    String message;
+}
