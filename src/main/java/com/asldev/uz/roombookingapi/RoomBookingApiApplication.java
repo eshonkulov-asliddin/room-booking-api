20a2e903012a8a4b46d94e0e65ec0a9a8ffd3164
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-06-12 09:13:20 +0500

    creating spring boot project

---
+++ src/main/java/com/asldev/uz/roombookingapi/RoomBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.asldev.uz.roombookingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomBookingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomBookingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asldev/uz/roombookingapi/RoomBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asldev.uz.roombookingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBookingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
