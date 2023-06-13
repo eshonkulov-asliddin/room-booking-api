4319d331a589154c74ac6ec47f6b2b3774000471
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-06-13 19:54:09 +0500

    changed class name

--- src/main/java/com/asldev/uz/roombookingapi/service/exception/ExceptionHandler.java
+++ src/main/java/com/asldev/uz/roombookingapi/service/exception/ExceptionHandler.java
@@ -3,15 +3,14 @@ package com.asldev.uz.roombookingapi.service.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionController {
-    @ExceptionHandler(NotFoundException.class)
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Object> handleException(NotFoundException exception){
         return new ResponseEntity<>(new ErrorMessage(exception.getMessage()), HttpStatus.NOT_FOUND);
     }
-    @ExceptionHandler(GoneException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(GoneException.class)
     public ResponseEntity<Object> handleException(GoneException exception){
         return new ResponseEntity<>(new ErrorMessage(exception.getMessage()), HttpStatus.GONE);
     }
