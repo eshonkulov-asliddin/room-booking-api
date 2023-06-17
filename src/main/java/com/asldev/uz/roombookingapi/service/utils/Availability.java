9ed4f6848d2c94d78e4afed0010d3f158bf35e9d
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-06-17 19:31:37 +0500

    added setter

--- src/main/java/com/asldev/uz/roombookingapi/service/utils/Availability.java
+++ src/main/java/com/asldev/uz/roombookingapi/service/utils/Availability.java
@@ -3,10 +3,12 @@ package com.asldev.uz.roombookingapi.service.utils;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Availability {
