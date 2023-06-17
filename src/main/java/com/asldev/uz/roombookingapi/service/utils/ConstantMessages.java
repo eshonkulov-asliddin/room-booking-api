9756dcbb95bbe5aba2311bba675b99e7835512ac
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-06-17 19:33:22 +0500

    fixed order

--- src/main/java/com/asldev/uz/roombookingapi/service/utils/ConstantMessages.java
+++ src/main/java/com/asldev/uz/roombookingapi/service/utils/ConstantMessages.java
@@ -4,7 +4,7 @@ public class ConstantMessages {
     public final static String NOT_FOUND = "topilmadi";
     public final static String NOT_AVAILABLE = "uzr, siz tanlagan vaqtda xona band";
     public final static String SUCCESS = "xona muvaffaqiyatli band qilindi";
-    public final static String LOCAL_DATE_TIME_FORMATTER = "dd-MM-yyyy HH:mm:ss";
     public final static String BAD_REQUEST = "kiritilgan parametrlar xato";
+    public final static String LOCAL_DATE_TIME_FORMATTER = "dd-MM-yyyy HH:mm:ss";
 
 }
