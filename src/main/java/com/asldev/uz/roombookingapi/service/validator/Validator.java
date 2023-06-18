c369041686dc15afb4e95bcf70802a07c72a131d
asliddin <eshonkulovasliddindeveloper@gmail.com>
2023-06-18 21:47:22 +0500

    introduces new validation for update request

--- src/main/java/com/asldev/uz/roombookingapi/service/validator/Validator.java
+++ src/main/java/com/asldev/uz/roombookingapi/service/validator/Validator.java
@@ -1,22 +1,43 @@
 package com.asldev.uz.roombookingapi.service.validator;
 
 import com.asldev.uz.roombookingapi.enums.RoomType;
-import com.asldev.uz.roombookingapi.service.dto.RoomDto;
+import com.asldev.uz.roombookingapi.service.dto.RoomDtoRequest;
+import com.asldev.uz.roombookingapi.service.dto.RoomDtoUpdate;
 import com.asldev.uz.roombookingapi.service.exception.ArgumentIsNotValidException;
 import com.asldev.uz.roombookingapi.service.utils.ConstantMessages;
 
 public class Validator {
-    public static void validate(RoomDto roomDto){
-        String name = roomDto.getName();
+    public static void validate(RoomDtoRequest roomDtoRequest){
+        String name = roomDtoRequest.getName();
         if (name == null || name.isEmpty()){
             throw new ArgumentIsNotValidException(ConstantMessages.BAD_REQUEST);
         }
-        RoomType type = roomDto.getType();
+        RoomType type = roomDtoRequest.getType();
         if (type == null){
             throw new ArgumentIsNotValidException(ConstantMessages.BAD_REQUEST);
         }
 
-        int capacity = roomDto.getCapacity();
+        int capacity = roomDtoRequest.getCapacity();
+        if (capacity <= 0){
+            throw new ArgumentIsNotValidException(ConstantMessages.BAD_REQUEST);
+        }
+    }
+
+    public static void validate(RoomDtoUpdate roomDtoRequest){
+        Long id = roomDtoRequest.getId();
+        if (id == null){
+            throw new ArgumentIsNotValidException(ConstantMessages.BAD_REQUEST);
+        }
+        String name = roomDtoRequest.getName();
+        if (name == null || name.isEmpty()){
+            throw new ArgumentIsNotValidException(ConstantMessages.BAD_REQUEST);
+        }
+        RoomType type = roomDtoRequest.getType();
+        if (type == null){
+            throw new ArgumentIsNotValidException(ConstantMessages.BAD_REQUEST);
+        }
+
+        int capacity = roomDtoRequest.getCapacity();
         if (capacity <= 0){
             throw new ArgumentIsNotValidException(ConstantMessages.BAD_REQUEST);
         }
