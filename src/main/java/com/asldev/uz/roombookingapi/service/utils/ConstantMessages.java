738c7c224d4118234e9eb081ee9b6176e534bce7
asliddin <eshonkulovasliddindeveloper@gmail.com>
2023-06-18 12:15:53 +0500

    added DELETE message

--- src/main/java/com/asldev/uz/roombookingapi/service/utils/ConstantMessages.java
+++ src/main/java/com/asldev/uz/roombookingapi/service/utils/ConstantMessages.java
@@ -5,6 +5,7 @@ public class ConstantMessages {
     public final static String NOT_AVAILABLE = "uzr, siz tanlagan vaqtda xona band";
     public final static String SUCCESS = "xona muvaffaqiyatli band qilindi";
     public final static String BAD_REQUEST = "kiritilgan parametrlar xato";
+    public final static String DELETE = "xona muvaffaqiyatli o'chirildi";
     public final static String LOCAL_DATE_TIME_FORMATTER = "dd-MM-yyyy HH:mm:ss";
 
 }
