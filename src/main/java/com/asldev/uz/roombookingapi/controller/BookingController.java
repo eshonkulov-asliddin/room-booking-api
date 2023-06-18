137c54abf09e7747e33423c763ca3a297ef35198
asliddin <eshonkulovasliddindeveloper@gmail.com>
2023-06-18 21:46:01 +0500

    documented controllers for swagger ui

--- src/main/java/com/asldev/uz/roombookingapi/controller/BookingController.java
+++ src/main/java/com/asldev/uz/roombookingapi/controller/BookingController.java
@@ -1,9 +1,15 @@
 package com.asldev.uz.roombookingapi.controller;
 
 import com.asldev.uz.roombookingapi.service.BookingService;
-import com.asldev.uz.roombookingapi.service.dto.BookingDto;
+import com.asldev.uz.roombookingapi.service.dto.BookingDtoRequest;
+import com.asldev.uz.roombookingapi.service.dto.BookingDtoResponse;
 import com.asldev.uz.roombookingapi.service.utils.SuccessMessage;
 import com.asldev.uz.roombookingapi.service.utils.Availability;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -16,6 +22,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/rooms")
+@Tag(name = "booking", description = "a booking API")
 public class BookingController {
     private final BookingService bookingService;
 
@@ -24,29 +31,50 @@ public class BookingController {
         this.bookingService = bookingService;
     }
 
+    @Operation(summary = "Get available booking times of room by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "404", description = "Room with given id is not found")
+    })
     @GetMapping("/{roomId}/availability")
-    private ResponseEntity<List<Availability>> getAvailableBookingTimes(@PathVariable Long roomId,
-                                                                       @RequestParam(name = "date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDate date)
+    private ResponseEntity<List<Availability>> getAvailableBookingTimes(@Parameter(description = "ID of room that need to fetched", required = true) @PathVariable Long id,
+                                                                       @Parameter(description = "Date for fetching available booking times") @RequestParam(name = "date", required = false)
+                                                                       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDate date)
     {
-        List<Availability> availableBookingTimes = bookingService.getAvailableBookingTimes(roomId, date);
+        List<Availability> availableBookingTimes = bookingService.getAvailableBookingTimes(id, date);
         return new ResponseEntity<>(availableBookingTimes, HttpStatus.OK);
     }
 
+    @Operation(summary = "Book a room with given id and booking obj")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful operation"),
+            @ApiResponse(responseCode = "404", description = "Room with given id is not found")
+    })
     @PostMapping("/{id}/book")
-    private ResponseEntity<SuccessMessage> bookRoom(@PathVariable Long id,
-                                                    @RequestBody BookingDto bookingDto) {
-        SuccessMessage successMessage = bookingService.bookRoom(id, bookingDto);
+    private ResponseEntity<SuccessMessage> bookRoom(@Parameter(description = "ID of room for booking", required = true) @PathVariable Long id,
+                                                    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Object for creating new Booking")
+                                                        @RequestBody BookingDtoRequest createRequest) {
+        SuccessMessage successMessage = bookingService.bookRoom(id, createRequest);
         return new ResponseEntity<>(successMessage, HttpStatus.CREATED);
     }
-
+    @Operation(summary = "Get all bookings")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "404", description = "Room with given id is not found")
+    })
     @GetMapping("/{id}/bookings")
-    private ResponseEntity<List<BookingDto>> findAll(@PathVariable Long id) {
-        List<BookingDto> all = bookingService.findAll(id);
+    private ResponseEntity<List<BookingDtoResponse>> findAll(@Parameter(description = "ID of room that is associated with booking", required = true) @PathVariable Long id) {
+        List<BookingDtoResponse> all = bookingService.findAll(id);
         return new ResponseEntity<>(all, HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete booking with given id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successful operation"),
+            @ApiResponse(responseCode = "404", description = "Room with given id is not found")
+    })
     @DeleteMapping("/bookings/{id}")
-    private void delete(@PathVariable Long id){
+    private void delete(@Parameter(description = "ID of booking that need to be deleted", required = true) @PathVariable Long id){
         bookingService.delete(id);
     }
 }
