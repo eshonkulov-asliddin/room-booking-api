01477dc2c1b6c657c191e53e43f038792d37828e
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2023-11-03 15:49:56 +0500

    Changed method name

--- src/main/java/com/asldev/uz/roombookingapi/repository/BookingRepository.java
+++ src/main/java/com/asldev/uz/roombookingapi/repository/BookingRepository.java
@@ -3,6 +3,8 @@ package com.asldev.uz.roombookingapi.repository;
 import com.asldev.uz.roombookingapi.repository.entity.Booking;
 import com.asldev.uz.roombookingapi.repository.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -14,6 +16,8 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     List<Booking> findAllByRoom(Room room);
 
-    List<Booking> findByRoomAndStartGreaterThanEqualAndEndLessThanEqualOrderByStart(Room room, LocalDateTime start, LocalDateTime end);
+    @Query("SELECT b FROM Booking b WHERE b.room = :room AND b.start >= :start AND b.end <= :end ORDER BY b.start")
+    List<Booking> findBookingsForRoomInPeriod(@Param("room") Room room, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+
 
 }
